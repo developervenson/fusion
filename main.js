cf2cff16cab1466d5a51f788c3adb3d01df06aa0
Workshop <workshop@Workshops-Mac-mini.local>
2026-03-14 22:46:31 +0400

    third version

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 //#endregion
+// #region Imports
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+// #endregion
+
+// #region Create and Mount App
+createApp(App).use(store).use(router).mount('#app');
+// #endregion
